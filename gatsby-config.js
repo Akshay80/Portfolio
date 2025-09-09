7928726755e502eef53c941110e9024cdf3d15ce
Akshay80 <68014140+Akshay80@users.noreply.github.com>
2025-09-09 22:43:02 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,13 @@ module.exports = {
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
-        icon: `static/favicon-32x32.png`
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          }
+        ],
       },
     },
     `gatsby-plugin-offline`,
